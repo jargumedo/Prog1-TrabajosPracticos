f59b1b464168fd604f83c8d2917d98b6deb88aa3
Juliano Argumedo <99204088+jargumedo@users.noreply.github.com>
2023-09-28 10:04:07 -0300

    Add files via upload

---
+++ ejercicio_uno.py
@@ -0,0 +1,49 @@
+#declarando variables numericas
+numero1 = 2
+numero2 = 1.8
+#realizando operaciones matematicas
+suma_nums = numero1+numero2
+resta_nums = round(numero1- numero2,2)
+mult_nums = numero1*numero2
+div_nums = round(numero1/numero2,2)
+#imprimiendo las variables
+print(f"La suma de los numeros es {suma_nums}, la resta {resta_nums}, la multiplicacion es {mult_nums} y la division es {div_nums}")
+#Ejercicio 5
+name_user = "Leandro"
+#Ejercicio 6
+precio_user = 5000
+#Ejercicio 7
+descuento = 0.25
+#Ejercicio 8 
+precio_final= precio_user - (precio_user*descuento)
+print(precio_final)
+#Ejercicio 9
+cadena = "Hello world"
+#Ejercicio 10
+longitud = len(cadena)
+print(longitud)
+#Ejercicio 11
+precio = int(2.2)
+print(precio)
+#Ejercicio 12
+nombre = "Juliano"
+apellido = "Argumedo"
+nombre_completo = nombre + " " + apellido
+print(nombre_completo)
+#Ejercicio 13
+edad = 21
+edad = edad+5
+edad = edad-10
+print(edad)
+#Ejercicio 14
+altura = 1.83
+altura = altura*4
+altura = altura/3
+print(altura)
+#Ejercicio 15
+name_mayuscula = "LEANDRO"
+name_min= name_mayuscula.lower()
+print(name_min)
+#Ejercicio 16
+name_capitalize = name_mayuscula.capitalize()
+print(name_capitalize)
